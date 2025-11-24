eefc9e034846cf61c60be714cf1b7d33f275b0ae
Caio Guimarães <profissional.caioguimaraes@gmail.com>
2025-11-24 16:48:13 -0300

    Initial Setup - Updated - ShadcnUI framework installed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
+import tailwindAnimate from 'tailwindcss-animate'
 
 const config = {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,33 +12,73 @@ const config = {
   theme: {
     extend: {
       colors: {
-        // brand colors
+        // --- TOKENS PERSONALIZADOS ---
         'brand-primary': 'var(--brand-color1)',
         'brand-secondary': 'var(--brand-color2)',
-
-        // (Background)
         'bg-main': 'var(--color-bg)',
         'bg-alt': 'var(--color-bg-alt)',
         'bg-element': 'var(--color-elements)',
-
-        // text colors
         'text-high': 'var(--color-text-high)',
         'text-low': 'var(--color-text-low)',
         'text-title': 'var(--color-titles)',
         white: 'var(--color-white)',
-
-        // neutral colors / elements
-        secondary: 'var(--color-secondary)',
         tertiary: 'var(--color-tertiary)',
         quaternary: 'var(--color-quaternary)',
+
+        // --- TOKENS DO SHADCN UI ---
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))',
+        },
       },
       fontFamily: {
         inter: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
         space: ['var(--font-space)', ...defaultTheme.fontFamily.serif],
       },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
     },
   },
-  plugins: [],
+  plugins: [tailwindAnimate], // applying
 }
 
 export default config
