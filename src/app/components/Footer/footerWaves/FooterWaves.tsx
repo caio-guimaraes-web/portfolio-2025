f419a47f60e5fd7e064a8777d845719d974c026e
Caio Guimarães <profissional.caioguimaraes@gmail.com>
2026-02-10 16:18:53 -0300

    Visual Structure - Footer - wave5.1 only tailwind

--- src/app/components/Footer/footerWaves/FooterWaves.tsx
+++ src/app/components/Footer/footerWaves/FooterWaves.tsx
@@ -1,14 +1,13 @@
 import dynamic from 'next/dynamic'
-import './waves.css'
 
 const WaveLayer = dynamic(() => import('./WaveLayer.client'), {
   ssr: false,
-  loading: () => <div className="wave-placeholder" />,
+  loading: () => <div className="w-full h-full" />,
 })
 
 export function FooterWaves() {
   return (
-    <div className="waves-wrapper">
+    <div className="relative w-full overflow-hidden h-[260px] md:h-[320px]">
       <WaveLayer />
     </div>
   )
