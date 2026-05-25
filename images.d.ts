de35c8be6a49a93d82a6aeb4c13b164c0951b51b
Caio Guimarães <profissional.caioguimaraes@gmail.com>
2026-05-25 16:40:36 -0300

    Updating - adding image types files and including on ts config

---
+++ images.d.ts
@@ -0,0 +1,18 @@
+import type { StaticImageData } from 'next/image'
+
+declare module '*.png' {
+  const value: StaticImageData
+  export default value
+}
+declare module '*.jpg' {
+  const value: StaticImageData
+  export default value
+}
+declare module '*.jpeg' {
+  const value: StaticImageData
+  export default value
+}
+declare module '*.svg' {
+  const value: StaticImageData
+  export default value
+}
